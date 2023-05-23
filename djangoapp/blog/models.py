60cf1d176c0a125305c577adc895b0e659ab7a78
kaleb <kalebclementino@gmail.com>
2023-05-23 17:57:06 -0300

    Criando o model e admin Page e Category

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -15,7 +15,55 @@ class Tag(models.Model):
         max_length=255,
     )
 
+    def __str__(self) -> str:
+        return self.name
+
     def save(self,*args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.name)
         return super().save(*args, **kwargs)
+    
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(
+        unique=True,
+        default=None,
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name)
+        return super().save(*args, **kwargs)
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
