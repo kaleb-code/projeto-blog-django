879c916a4f4ddc261ad0842fb815663c185ade4d
kaleb <kalebclementino@gmail.com>
2023-05-25 08:00:36 -0300

    Configurando o django-summernote como editor HTML em TextField nos models Django

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -2,6 +2,26 @@ from django.db import models
 from utils.rands import slugify_new
 from django.contrib.auth.models import User
 from utils.images import resize_image
+from django_summernote.models import AbstractAttachment
+
+class PostAttachment(AbstractAttachment):
+    def save(self,*args, **kwargs):
+        if not self.name:
+            self.name = self.file.name
+
+        current_file_name = str(self.file.name)
+        super_save = super().save(*args, **kwargs)
+        file_changed = False
+
+        if self.file:
+            file_changed = current_file_name != self.file.name
+
+        if file_changed:
+            resize_image(self.file,900,True,70)
+
+        return super_save
+
+
 # Create your models here.
 class Tag(models.Model):
     class Meta:
@@ -89,7 +109,7 @@ class Post(models.Model):
         ),
     )
     content = models.TextField()
-    cover = models.ImageField(upload_to='post/%Y/%m',blank=True,default='')
+    file = models.ImageField(upload_to='post/%Y/%m',blank=True,default='')
     cover_in_post_content = models.BooleanField(
         default=True,
         help_text='Se marcado,exibirá a capa dentro do post'
@@ -122,22 +142,15 @@ class Post(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.title, 4)
 
-        current_favicon_name = str(self.cover.name)
+        current_favicon_name = str(self.file.name)
         super_save = super().save(*args, **kwargs)
         favicon_changed = False
 
-        if self.cover:
-            favicon_changed = current_favicon_name != self.cover.name
+        if self.file:
+            favicon_changed = current_favicon_name != self.file.name
 
         if favicon_changed:
-            print()
-            print()
-            print()
-            print('cover change')
-            print()
-            print()
-            print()
-            resize_image(self.cover,900,True,70)
+            resize_image(self.file,900,True,70)
 
         return super_save
 
